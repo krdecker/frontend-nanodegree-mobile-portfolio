ff758042f21d03be2dc8b25381e6cab3d51ca649
krdecker <krd.345@gmail.com>
2015-09-26 15:24:23 -0700

    add uglify task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,27 @@
-# Gruntfile.js
+// Gruntfile.js
 
-module.exports = function(grunt) {
+module.exports = function(grunt) { // Project configuration.
+    // krd comment - feed a configuration json into the initConfig method of grunt function
+
+    grunt.initConfig(
+        { //this is the config object between these curly braces
+            pkg: grunt.file.readJSON('package.json'),
+            uglify: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                },
+                build: {
+                    src: 'js/perfmatters.js',
+                    dest: 'js/perfmatters.min.js'
+                }
+            }
+        }
+    );
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
 
 };
